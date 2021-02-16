329866e0c77a0a0657150f7684bbe150546596ad
Chris Stinemetz <chris.stinemetz@gmail.com>
2021-02-16 17:27:50 -0600

    added components

--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout'
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <Layout>
+    <h1>hello from gatsby</h1>
+    <h1>hello world</h1>
+    <h1>hello people</h1>
+  </Layout>
+)
